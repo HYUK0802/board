719df043bd1ec833613d41f4d64cefa17a198670
hyuk <tig05205@naver.com>
2024-02-19 21:51:56 +0900

    board1

--- src/main/java/com/hyuk/board/controller/BoardController.java
+++ src/main/java/com/hyuk/board/controller/BoardController.java
@@ -1,13 +1,21 @@
 package com.hyuk.board.controller;
 
+import com.hyuk.board.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/board")
+@RequiredArgsConstructor
 public class BoardController {
 
+    private final PostService postService;
 
-
-
+    // 게시글 작성 페이지
+    @GetMapping("/post/write.do")
+    public String openPostWrite(Model model) {
+        return "post/write";
+    }
 }
