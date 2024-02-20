daa51164a600a316367c93e2718562fb7cd40d3b
hyuk <tig05205@naver.com>
2024-02-20 19:27:49 +0900

    board1

--- src/main/java/com/hyuk/board/controller/BoardController.java
+++ src/main/java/com/hyuk/board/controller/BoardController.java
@@ -1,11 +1,17 @@
 package com.hyuk.board.controller;
 
+import com.hyuk.board.entity.PostRequest;
+import com.hyuk.board.entity.PostResponse;
 import com.hyuk.board.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -15,7 +21,24 @@ public class BoardController {
 
     // 게시글 작성 페이지
     @GetMapping("/post/write.do")
-    public String openPostWrite(Model model) {
+    public String openPostWrite(@RequestParam(value = "id", required = false) final Long id, Model model) {
+        if (id != null) {
+            PostResponse post = postService.findPostById(id);
+            model.addAttribute("post", post);
+        }
         return "post/write";
+    }
+
+    @GetMapping("/post/list.do")
+    public String openPostList(Model model) {
+        List<PostResponse> posts = postService.findAllPost();
+        model.addAttribute("posts", posts);
+        return "post/list";
+    }
+
+    @PostMapping("/post/save.do")
+    public String savePost(final PostRequest params) {
+        postService.savePost(params);
+        return "redirect:/post/list.do";
     }
 }
