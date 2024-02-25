285819e69ec0a1dfac5c9e61a58702881d8575f4
hyuk <tig05205@naver.com>
2024-02-25 17:12:09 +0900

    interceptor

---
+++ src/main/java/com/hyuk/board/config/WebMvcConfig.java
@@ -0,0 +1,14 @@
+package com.hyuk.board.config;
+
+import com.hyuk.board.interceptor.LoggerInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor())
+                .excludePathPatterns("/css/**", "/images/**", "/js/**");
+    }
+}
