90ee107f8bb291bf4a58b7837e1388577e71bb0a
hyuk <tig05205@naver.com>
2024-02-26 19:38:45 +0900

    search

--- src/main/java/com/hyuk/board/controller/BoardController.java
+++ src/main/java/com/hyuk/board/controller/BoardController.java
@@ -11,7 +11,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Controller
 @RequiredArgsConstructor
@@ -61,9 +64,9 @@ public class BoardController {
 
     // 게시글 삭제
     @PostMapping("/post/delete.do")
-    public String deletePost(@RequestParam final Long id, Model model) {
+    public String deletePost(@RequestParam final Long id,final SearchDto queryParams, Model model) {
         postService.deletePost(id);
-        MessageDto message = new MessageDto("게시글 삭제가 완료되었습니다.", "/post/list.do", RequestMethod.GET, null);
+        MessageDto message = new MessageDto("게시글 삭제가 완료되었습니다.", "/post/list.do", RequestMethod.GET, queryParamsToMap(queryParams));
         return showMessageAndRedirect(message, model);
     }
 
@@ -71,5 +74,16 @@ public class BoardController {
     private String showMessageAndRedirect(final MessageDto params, Model model) {
         model.addAttribute("params", params);
         return "common/messageRedirect";
+    }
+
+    // 쿼리 스트링 파라미터를 Map에 담아 반환
+    private Map<String, Object> queryParamsToMap(final SearchDto queryParams) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("page", queryParams.getPage());
+        data.put("recordSize", queryParams.getRecordSize());
+        data.put("pageSize", queryParams.getPageSize());
+        data.put("keyword", queryParams.getKeyword());
+        data.put("searchType", queryParams.getSearchType());
+        return data;
     }
 }
--- src/main/java/com/hyuk/board/dto/MessageDto.java
+++ src/main/java/com/hyuk/board/dto/MessageDto.java
@@ -14,5 +14,5 @@ public class MessageDto {
     private String message;             // 사용자에게 전달할 메시지
     private String redirectUri;         // 리다이렉트 URI
     private RequestMethod method;       // HTTP 요청 메서드
-    private Map<String, Objects> data;  // 화면(view)으로 전달할 데이터(파라미터)
+    private Map<String, Object> data;  // 화면(view)으로 전달할 데이터(파라미터)
 }
