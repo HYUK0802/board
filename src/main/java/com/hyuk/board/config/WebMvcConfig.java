c16ddffe7ddea236e1e85ba28f3d37e324e4c278
hyuk <tig05205@naver.com>
2024-03-12 00:57:40 +0900

    login (1/2)

--- src/main/java/com/hyuk/board/config/WebMvcConfig.java
+++ src/main/java/com/hyuk/board/config/WebMvcConfig.java
@@ -1,14 +1,21 @@
 package com.hyuk.board.config;
 
 import com.hyuk.board.interceptor.LoggerInterceptor;
+import com.hyuk.board.interceptor.LoginCheckInterceptor;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoggerInterceptor())
                 .excludePathPatterns("/css/**", "/images/**", "/js/**");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .addPathPatterns("/**/*.do")
+                .excludePathPatterns("/log*");
     }
 }
--- src/main/java/com/hyuk/board/member/MemberController.java
+++ src/main/java/com/hyuk/board/member/MemberController.java
@@ -1,5 +1,7 @@
 package com.hyuk.board.member;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -51,4 +53,29 @@ public class MemberController {
         return memberService.countMemberByLoginId(loginId);
     }
 
+    // 로그인
+    @PostMapping("/login")
+    @ResponseBody
+    public MemberResponse login(HttpServletRequest request) {
+
+        // 1. 회원 정보 조회
+        String loginId = request.getParameter("loginId");
+        String password = request.getParameter("password");
+        MemberResponse member = memberService.login(loginId, password);
+
+        // 2. 세션에 회원 정보 저장 & 세션 유지 시간 설정
+        if (member != null) {
+            HttpSession session = request.getSession();
+            session.setAttribute("loginMember", member);
+            session.setMaxInactiveInterval(60 * 30);
+        }
+        return member;
+    }
+
+    // 로그아웃
+    @PostMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/login.do";
+    }
 }
